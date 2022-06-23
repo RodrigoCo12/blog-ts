4c13431d8809c70e17dd559d86f4a7a50a0419aa
rodrigo corrales <corralesrodrigo201202@gmail.com>
2022-06-23 00:48:11 -0400

    error

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -57,7 +57,7 @@ const PostDetail = ({ post }) => {
             <div className="mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
                 <div className="relative mb-6 overflow-hidden shadow-md">
                     <img
-                        src={post.featuredImage.url}
+                        // src={post.featuredImage.url}
                         alt=""
                         className="h-full w-full rounded-t-lg object-cover  object-top shadow-lg lg:rounded-lg"
                     />
@@ -66,12 +66,12 @@ const PostDetail = ({ post }) => {
                     <div className="mb-8 flex w-full items-center">
                         <div className="mr-8 hidden items-center justify-center md:flex lg:mb-0 lg:w-auto">
                             <img
-                                alt={post.author.name}
+                                // alt={post.author.name}
                                 className="h-7 w-7 rounded-full align-middle"
-                                src={post.author.photo.url}
+                                // src={post.author.photo.url}
                             />
                             <p className="ml-2 inline align-middle text-lg font-medium text-gray-700">
-                                {post.author.name}
+                                {/* {post.author.name} */}
                             </p>
                         </div>
                         <div className="font-medium text-gray-700">
@@ -89,17 +89,17 @@ const PostDetail = ({ post }) => {
                                     d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                                 />
                             </svg>
-                            <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+                            {/* <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span> */}
                         </div>
                     </div>
-                    <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                    {/* <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
                     {post.content.raw.children.map((typeObj, index) => {
                         const children = typeObj.children.map((item, itemindex) =>
                             getContentFragment(itemindex, item.text, item)
                         );
 
                         return getContentFragment(index, children, typeObj, typeObj.type);
-                    })}
+                    })} */}
                 </div>
             </div>
         </>
