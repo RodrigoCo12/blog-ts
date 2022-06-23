14676a2b19bc5a95a6ddbf5ab8291fca9617e53f
rodrigo corrales <corralesrodrigo201202@gmail.com>
2022-06-23 19:50:12 -0400

    vercel test

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -57,7 +57,7 @@ const PostDetail = ({ post }) => {
             <div className="mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
                 <div className="relative mb-6 overflow-hidden shadow-md">
                     <img
-                        // src={post.featuredImage.url}
+                        src={post.featuredImage.url}
                         alt=""
                         className="h-full w-full rounded-t-lg object-cover  object-top shadow-lg lg:rounded-lg"
                     />
@@ -66,12 +66,12 @@ const PostDetail = ({ post }) => {
                     <div className="mb-8 flex w-full items-center">
                         <div className="mr-8 hidden items-center justify-center md:flex lg:mb-0 lg:w-auto">
                             <img
-                                // alt={post.author.name}
+                                alt={post.author.name}
                                 className="h-7 w-7 rounded-full align-middle"
-                                // src={post.author.photo.url}
+                                src={post.author.photo.url}
                             />
                             <p className="ml-2 inline align-middle text-lg font-medium text-gray-700">
-                                {/* {post.author.name} */}
+                                {post.author.name}
                             </p>
                         </div>
                         <div className="font-medium text-gray-700">
@@ -79,27 +79,24 @@ const PostDetail = ({ post }) => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="mr-2 inline h-7 w-7 text-pink-500"
                                 fill="none"
-                                viewBox="0 0 24 24"
                                 stroke="currentColor"
                             >
                                 <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    d="M22.5,3H21V2a1,1,0,0,0-1-1H19a1,1,0,0,0-1,1V3H14V2a1,1,0,0,0-1-1H12a1,1,0,0,0-1,1V3H7V2A1,1,0,0,0,6,1H5A1,1,0,0,0,4,2V3H2.5A1.5,1.5,0,0,0,1,4.5v18A1.5,1.5,0,0,0,2.5,24h20A1.5,1.5,0,0,0,24,22.5V4.5A1.5,1.5,0,0,0,22.5,3ZM19,2l1,0,0,3L19,5ZM12,2l1,0V3.44s0,0,0,.06,0,0,0,.07L13,5,12,5ZM5,2,6,2,6,5,5,5ZM2.5,4H4V5A1,1,0,0,0,5,6H6A1,1,0,0,0,7,5V4h4V5a1,1,0,0,0,1,1H13a1,1,0,0,0,1-1V4h4V5a1,1,0,0,0,1,1H20a1,1,0,0,0,1-1V4h1.5a.5.5,0,0,1,.5.5V8H2V4.5A.5.5,0,0,1,2.5,4Zm20,19H2.5a.5.5,0,0,1-.5-.5V9H23V22.5A.5.5,0,0,1,22.5,23Z"
                                 />
                             </svg>
-                            {/* <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span> */}
+                            <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
                         </div>
                     </div>
-                    {/* <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                    <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
                     {post.content.raw.children.map((typeObj, index) => {
                         const children = typeObj.children.map((item, itemindex) =>
                             getContentFragment(itemindex, item.text, item)
                         );
 
                         return getContentFragment(index, children, typeObj, typeObj.type);
-                    })} */}
+                    })}
                 </div>
             </div>
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = ({ posts }: any) => {
             <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
                 <div className="col-span-1 lg:col-span-8">
                     {posts.map((post: any) => (
-                        <PostCard post={post.node} key={post.title} />
+                        <PostCard post={post.node} key={post.node.title} />
                     ))}
                 </div>
                 <div className="lg-col-span-4 col-span-4">
