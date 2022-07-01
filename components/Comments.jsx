315a969a7c23e50bb341e737a6c71883581962d9
rodrigo corrales <corralesrodrigo201202@gmail.com>
2022-07-01 19:59:12 -0400

    comments component

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+import { getComments } from '../services'
 
-const Comments = () => {
-    return <h2>Comments</h2>;
-};
+const Comments = ({ slug }) => {
+  const [comment, setComment] = useState([])
 
-export default Comments;
+  useEffect(() => {
+    getComments(slug).then((result) => setComment(result))
+  }, [])
+
+  return (
+    <>
+      {comment.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+            {comment.length} Comments
+          </h3>
+          {comment.map((comment) => (
+            <div
+              key={comment.createdAt}
+              className="mb-4 border-b border-gray-100 pb-4"
+            >
+              <p>
+                <span className="font-semibold"> {comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('DD MMM, YYYY')}
+              </p>
+              <p className="w-full whitespace-pre-line text-gray-600">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Comments
